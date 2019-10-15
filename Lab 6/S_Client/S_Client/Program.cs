53dc7112e731ce9bcdcdc4671f8eddd7a1620c17
durlabhashok <55022250+durlabhashok@users.noreply.github.com>
2019-10-15 14:48:10 +0500

    Add files via upload

---
+++ Lab 6/S_Client/S_Client/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.IO;
+
+
+namespace S_Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool status = true;
+            TcpClient socketForServer;
+            try
+            {
+                socketForServer = new TcpClient("localhost",8080);
+                Console.WriteLine("Connected with Server...");
+
+
+            }
+            catch
+            {
+                
+                 Console.WriteLine("Failed to connect");
+                 return;
+            }
+
+            NetworkStream networkStream = socketForServer.GetStream();
+            StreamReader reader = new StreamReader(networkStream);
+            StreamWriter writer = new StreamWriter(networkStream);
+
+            try
+            {
+                string servermsg = "";
+                string clientmsg = "";
+
+                while (status)
+                {
+                    Console.WriteLine("Client: ");
+                    clientmsg = Console.ReadLine();
+
+                    if (clientmsg == "bye" || clientmsg == "Bye")
+                    {
+                        status = false;
+
+                        writer.WriteLine("bye");
+                        writer.Flush();
+
+                        return;
+                    }
+                    else
+                    {
+                        writer.WriteLine(clientmsg);
+                        writer.Flush();
+                        servermsg = reader.ReadLine();
+                        Console.WriteLine("Server: " + servermsg);
+                    }
+                }
+
+            }
+            catch
+            {
+
+                Console.WriteLine("Error from server app");
+            }
+            reader.Close();
+            writer.Close();
+            networkStream.Close();
+
+        }
+    }
+}
---
+++ Lab 6/S_socket/S_socket/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace S_socket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                bool status = true;
+                string servermsg = "";
+                string clientmsg = "";
+
+                TcpListener tcplsitner = new TcpListener(8080);
+                tcplsitner.Start();
+                Console.WriteLine("Server Running..");
+
+                Socket socketForClient = tcplsitner.AcceptSocket();
+                Console.WriteLine("Client Accepted...");
+                NetworkStream networkStream = new NetworkStream(socketForClient);
+
+                StreamWriter writer = new StreamWriter(networkStream);
+                StreamReader reader = new StreamReader(networkStream);
+
+                while (status)
+                {
+                    if (socketForClient.Connected)
+                    {
+                        servermsg = reader.ReadLine();
+                        Console.WriteLine("Client: "+servermsg);
+                        if (servermsg=="bye" || servermsg=="Bye")
+                        {
+                            reader.Close(); 
+                            writer.Close(); 
+                            networkStream.Close();
+
+                            return;
+                        }
+                        Console.WriteLine("Server:");
+                        clientmsg = Console.ReadLine();
+                        writer.WriteLine(clientmsg);
+                        writer.Flush();
+                    }
+                }
+
+                reader.Close();
+                writer.Close();
+                networkStream.Close();
+                Console.WriteLine("Exiting Application");
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+                
+            }
+        }
+    }
+}
